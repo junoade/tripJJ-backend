d7a2beb5c4e02785e227073a012352f7aac33d8a
jjho311 <jjho311@naver.com>
2023-11-10 15:45:28 +0900

    add: ReplyService, BoardQnaService

---
+++ src/main/java/com/trip/qna/service/BoardQnaServiceImpl.java
@@ -0,0 +1,49 @@
+package com.trip.qna.service;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.trip.qna.BoardQnaDto;
+import com.trip.qna.dao.BoardQnaDao;
+
+@Service
+public class BoardQnaServiceImpl implements BoardQnaService {
+
+	@Autowired
+	private BoardQnaDao dao;
+	
+	@Override
+	public int registerQna(BoardQnaDto boardQnaDto) throws SQLException {
+		return dao.registerQna(boardQnaDto);
+	}
+
+	@Override
+	public List<BoardQnaDto> listQna(Map<String, Object> param) throws SQLException {
+		return dao.listQna(param);
+	}
+
+	@Override
+	public BoardQnaDto getQna(int articleNo) throws SQLException {
+		return getQna(articleNo);
+	}
+
+	@Override
+	public void updateHit(int articleNo) throws SQLException {
+		dao.updateHit(articleNo);
+
+	}
+
+	@Override
+	public void modifyQna(BoardQnaDto boardQnaDto) throws SQLException {
+		dao.modifyQna(boardQnaDto);
+
+	}
+
+	@Override
+	public void deleteQna(int articleNo) throws SQLException {
+		dao.deleteQna(articleNo);
+	}
+
+}
--- src/main/java/com/trip/qna/service/ReplyService.java
+++ src/main/java/com/trip/qna/service/ReplyService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import com.trip.qna.ReplyDto;
 
-@Mapper
 public interface ReplyService {
 	
 //	등록
